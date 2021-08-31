fdbd684374d92db4ed998c34309d901dd6a4731b
Melvic Ybanez <melvic.ybanez2014@gmail.com>
2021-09-01 04:03:10 +0800

    Factor functions for tuples

--- src/main/scala/com/melvic/esena/ETuple.scala
+++ src/main/scala/com/melvic/esena/ETuple.scala
@@ -6,4 +6,10 @@ object ETuple {
   def isPoint(tuple: ETuple): Boolean = tuple.w == 1
 
   def isVector(tuple: ETuple): Boolean = tuple.w == 0
+
+  def point(x: Double, y: Double, z: Double): ETuple =
+    ETuple(x, y, z, 1)
+
+  def vector(x: Double, y: Double, z: Double): ETuple =
+    ETuple(x, y, z, 0)
 }
--- src/test/scala/TuplesSpec.scala
+++ src/test/scala/TuplesSpec.scala
@@ -13,6 +13,7 @@ class TuplesSpec extends AnyFlatSpec with should.Matchers {
     assert(ETuple.isPoint(a))
     assert(!ETuple.isVector(a))
   }
+
   "A vector" should "have w=0.0" in {
     val a = ETuple(4.3, -4.2, 3.1, 0.0)
     a.x should be (4.3)
@@ -21,5 +22,15 @@ class TuplesSpec extends AnyFlatSpec with should.Matchers {
     a.w should be (0.0)
     assert(!ETuple.isPoint(a))
     assert(ETuple.isVector(a))
+  }
+
+  "point()" should "create tuple with w=1.0" in {
+    val p = ETuple.point(4, -4, 3)
+    p should be (ETuple(4, -4, 3, 1))
+  }
+
+  "vector()" should "create tuple with w=0.0"  in {
+    val v = ETuple.vector(4, -4, 3)
+    v should be (ETuple(4, -4, 3, 0))
   }
 }
