894d62d62ffbb58b68de8a2d6206ee25e4887fa9
Melvic Ybanez <melvic.ybanez2014@gmail.com>
2021-09-04 05:05:40 +0800

    Hadamard products

--- src/main/scala/com/melvic/esena/Color.scala
+++ src/main/scala/com/melvic/esena/Color.scala
@@ -5,4 +5,16 @@ final case class Color(red: Double, green: Double, blue: Double) extends Tuple {
   override val y = green
   override val z = blue
   override val w = 0
+
+  def hadamardProduct(that: Color): Color =
+    Color(
+      red * that.red,
+      green * that.green,
+      blue * that.blue
+    )
+
+  /**
+   * An alias for the [[hadamardProduct]]
+   */
+  def *(that: Color): Color = hadamardProduct(that)
 }
---
+++ src/test/scala/ColorSpec.scala
@@ -0,0 +1,28 @@
+import com.melvic.esena.Color
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should
+
+class ColorSpec extends AnyFlatSpec with should.Matchers {
+  "Colors" should "be represented as RGB tuples" in {
+    val c = Color(-0.5, 0.4, 1.7)
+    c.red should be (-0.5)
+    c.green should be (0.4)
+    c.blue should be (1.7)
+  }
+
+  "Adding colors" should "be similar to adding tuples" in {
+    Color(0.9, 0.6, 0.75) + Color(0.7, 0.1, 0.25) should be (Color(1.6, 0.7, 1.0))
+  }
+
+  "Subtracting colors" should "be similar to subtracting tuples" in {
+    Color(0.9, 0.6, 0.75) - Color(0.7, 0.1, 0.25) should be (Color(0.2, 0.5, 0.5))
+  }
+
+  "Multiplying a color by a scalar" should "be similar to multiplying a tuple by a scalar" in {
+    Color(0.2, 0.3, 0.4) * 2.0 should be (Color(0.4, 0.6, 0.8))
+  }
+
+  "Multiplying colors" should "compute their hadamard products" in {
+    Color(1, 0.2, 0.4) * Color(0.9, 1, 0.1) should be (Color(0.9, 0.2, 0.04))
+  }
+}
