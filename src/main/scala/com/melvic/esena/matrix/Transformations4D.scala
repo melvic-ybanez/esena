d7da88f34200fb037062389d2929741791655258
Melvic Ybanez <melvic.ybanez2014@gmail.com>
2021-09-09 07:03:37 +0800

    Add doc comments

--- src/main/scala/com/melvic/esena/matrix/Transformations4D.scala
+++ src/main/scala/com/melvic/esena/matrix/Transformations4D.scala
@@ -1,9 +1,30 @@
 package com.melvic.esena.matrix
 
 trait Transformations4D {
+
+  /**
+    * Increases each of the elements of a point by multiplying
+    * it with the following matrix:
+    * [ 1 0 0 x ]
+    * [ 0 1 0 y ]
+    * [ 0 0 1 z ]
+    * [ 0 0 0 1 ]
+    * Note: Applying this to a vector shouldn't alter the vector
+    * because the vector's fourth component is 0 (it cancels the fourth
+    * column)
+    */
   def translation(x: Double, y: Double, z: Double): Matrix =
     Matrix.identity4By4(0, 3, x)(1, 3, y)(2, 3, z)
 
+  /**
+    * Multiplies each component of a tuple by its corresponding scalar.
+    * Scaling matrix:
+    * [ x, 0, 0, 0 ]
+    * [ 0, y, 0, 0 ]
+    * [ 0, 0, z, 0 ]
+    * [ 0, 0, 0, 1 ]
+    * This works for both point and vector.
+    */
   def scaling(x: Double, y: Double, z: Double): Matrix =
     Matrix.identity4By4(0, 0, x)(1, 1, y)(2, 2, z)
 
@@ -24,9 +45,9 @@ trait Transformations4D {
   }
 
   /**
-   * Clockwise rotations along the y-axis.
-   * Note: This rotation is based on the left-hand rule.
-   */
+    * Clockwise rotations along the y-axis.
+    * Note: This rotation is based on the left-hand rule.
+    */
   def rotationY(radian: Double): Matrix = {
     val cos = math.cos(radian)
     val sin = math.sin(radian)
@@ -40,9 +61,9 @@ trait Transformations4D {
   }
 
   /**
-   * Clockwise rotations along the z-axis.
-   * Note: This rotation is based on the left-hand rule.
-   */
+    * Clockwise rotations along the z-axis.
+    * Note: This rotation is based on the left-hand rule.
+    */
   def rotationZ(radian: Double): Matrix = {
     val cos = math.cos(radian)
     val sin = math.sin(radian)
