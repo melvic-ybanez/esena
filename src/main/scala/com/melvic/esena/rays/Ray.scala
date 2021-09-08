fca5a9610099d2710b95e18ea478de1fd4062688
Melvic Ybanez <melvic.ybanez2014@gmail.com>
2021-09-09 07:47:03 +0800

    Compute points along a ray

--- src/main/scala/com/melvic/esena/rays/Ray.scala
+++ src/main/scala/com/melvic/esena/rays/Ray.scala
@@ -1,5 +1,10 @@
 package com.melvic.esena.rays
 
-import com.melvic.esena.tuples.{Point, Vec}
+import com.melvic.esena.tuples.{Point, Tuple, Vec}
 
-final case class Ray(origin: Point, direction: Vec)
+final case class Ray(origin: Point, direction: Vec) {
+  def position(t: Double): Tuple =
+    origin + direction * t
+
+  def apply(t: Double): Tuple = position(t)
+}
--- src/test/scala/RaysSpec.scala
+++ src/test/scala/RaysSpec.scala
@@ -11,4 +11,12 @@ class RaysSpec extends AnyFlatSpec with should.Matchers {
     ray.origin should be (origin)
     ray.direction should be (direction)
   }
+
+  it should "be able to compute a point from a distance" in {
+    val r = Ray(Point(2, 3, 4), Vec(1, 0, 0))
+    r(0) should be (Point(2, 3, 4))
+    r(1) should be (Point(3, 3, 4))
+    r(-1) should be (Point(1, 3, 4))
+    r(2.5) should be (Point(4.5, 3, 4))
+  }
 }
