7edbdf5f0fb1ed1b0d028f7ca4f0e43c6bfcabad
Melvic Ybanez <melvic.ybanez2014@gmail.com>
2021-09-10 01:40:50 +0800

    Aggregating intersections

--- src/main/scala/com/melvic/esena/rays/Intersection.scala
+++ src/main/scala/com/melvic/esena/rays/Intersection.scala
@@ -3,3 +3,8 @@ package com.melvic.esena.rays
 import com.melvic.esena.shapes.Shape
 
 final case class Intersection(t: Double, obj: Shape)
+
+object Intersection {
+  def aggregate(intersection: Intersection*): Vector[Intersection] =
+    intersection.toVector
+}
--- src/test/scala/IntersectionsSpec.scala
+++ src/test/scala/IntersectionsSpec.scala
@@ -4,8 +4,20 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
 
 class IntersectionsSpec extends AnyFlatSpec with should.Matchers {
-  val s = new Sphere()
-  val i = Intersection(3.5, s)
-  i.t should be (3.5)
-  i.obj should be (s)
+  "An intersection" should "encapsulates t and object" in {
+    val s = new Sphere()
+    val i = Intersection(3.5, s)
+    i.t should be (3.5)
+    i.obj should be (s)
+  }
+
+  "Integrations" should "be able to aggregate" in {
+    val s = new Sphere
+    val i1 = Intersection(1, s)
+    val i2 = Intersection(2, s)
+    val xs = Intersection.aggregate(i1, i2)
+    xs.size should be (2)
+    xs(0).t should be (1)
+    xs(1).t should be (2)
+  }
 }
