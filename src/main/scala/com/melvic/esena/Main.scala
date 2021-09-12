0566b8f82d569f76d71d486494a5f1d10e51d0a3
Melvic Ybanez <melvic.ybanez2014@gmail.com>
2021-09-13 02:15:58 +0800

    sample world

--- src/main/scala/com/melvic/esena/Main.scala
+++ src/main/scala/com/melvic/esena/Main.scala
@@ -5,53 +5,44 @@ import java.nio.file.{Files, Paths}
 
 import com.melvic.esena.canvas.{Canvas, Color}
 import com.melvic.esena.lights.{Material, PointLight, lighting}
+import com.melvic.esena.matrix.{rotationX, rotationY, scaling, translation, view}
 import com.melvic.esena.rays.{Intersection, Ray}
+import com.melvic.esena.scene.{Camera, World}
 import com.melvic.esena.shapes.Sphere
-import com.melvic.esena.tuples.Point
+import com.melvic.esena.tuples.{Point, Vec}
 
 object Main {
   def main(args: Array[String]): Unit = {
-    // This is just a sample rendered shape for now
-    val rayOrigin = Point(0, 0, -5)
-    val wallZ = 10
-    val wallSize = 7.0
-    val canvasPixels = 500
-    val pixelSize = wallSize / canvasPixels
-    val half = wallSize / 2
+    // This is just a sample rendered world for now
+    val floor = Sphere()
+      .transform(scaling(10, 0.01, 10))
+      .withMaterial(Material(color = Color(1, 0.9, 0.9), specular = 0))
+    val leftWall = Sphere()
+      .transform(translation(0, 0, 5) * rotationY(-math.Pi / 4) * rotationX(math.Pi / 2) * scaling(10, 0.01, 10))
+      .withMaterial(floor.material)
+    val rightWall = Sphere()
+      .transform(translation(0, 0, 5) * rotationY(math.Pi / 4) * rotationX(math.Pi / 2) * scaling(10, 0.01, 10))
+      .withMaterial(floor.material)
+    val middleSphere = Sphere()
+      .transform(translation(-0.5, 1, 0.5))
+      .withMaterial(Material(color = Color(0.1, 1, 0.5), diffuse = 0.7, specular = 0.3))
+    val leftSphere = Sphere()
+      .transform(translation(-1.5, 0.33, -0.75) * scaling(0.33, 0.33, 0.33))
+      .withMaterial(Material(color = Color(1, 0.8, 0.1), diffuse = 0.7, specular = 0.3))
 
-    // sample canvas
-    val canvas = Canvas(canvasPixels, canvasPixels)
-    val shape = Sphere().withMaterial(Material(color = Color(1, 0.2, 1)))
-
-    val light = PointLight(Point(-10, 10, -10), Color.White)
-
-    // draw the object on the canvas
-    val updatedCanvas = (0 until canvasPixels).foldLeft(canvas) { (canvas, y) =>
-      val worldY = half - pixelSize * y
-      (0 until canvasPixels).foldLeft(canvas) { (canvas, x) =>
-        val worldX = -half + pixelSize * x
-        val position = Point(worldX, worldY, wallZ)
-
-        val ray = Ray(rayOrigin, (position - rayOrigin).normalize)
-        val xs = shape.intersect(ray)
-        val hits = Intersection.hit(xs)
-
-        if (hits.nonEmpty) {
-          val hit = hits.head
-          val point = ray.position(hit.t)
-          val normal = hit.obj.normalAt(point)
-          val eye = -ray.direction
-
-          val color = lighting(hit.obj.material, light, point, eye, normal)
-
-          canvas.writePixel(x, y, color)
-        } else canvas
-      }
+    val moreSmallSpheres = (0 until 5).map { i =>
+      val scale = 1 - (i / 10.0)
+      leftSphere.addTransformation(translation(i, 0, 0) * scaling(scale, scale, scale))
     }
 
-    println(updatedCanvas.ppm.toString)
+    // white light source, from above and to the left
+    val world = World.default.withLight(PointLight(Point(-10, 10, -10), Color.White))
+      .copy(objects = Vector(floor, leftWall, rightWall, middleSphere, leftSphere) ++ moreSmallSpheres)
+
+    val cam = Camera(800, 400, math.Pi / 3).transform(view(Point(0, 1.5, -5), Point(0, 1, 0), Vec(0, 1, 0)))
+    val canvas = cam.render(world)
 
     // write to file
-    Files.write(Paths.get("sample.ppm"), updatedCanvas.ppm.toString.getBytes(StandardCharsets.UTF_8))
+    Files.write(Paths.get("sample.ppm"), canvas.ppm.toString.getBytes(StandardCharsets.UTF_8))
   }
 }
