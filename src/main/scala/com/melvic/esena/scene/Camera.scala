b01a9730535540a8a5c554aecd1dbcab42d01385
Melvic Ybanez <melvic.ybanez2014@gmail.com>
2021-09-12 21:01:55 +0800

    Compute camera pixel size

--- src/main/scala/com/melvic/esena/scene/Camera.scala
+++ src/main/scala/com/melvic/esena/scene/Camera.scala
@@ -8,4 +8,27 @@ final case class Camera(
     vSize: Double,
     fieldOfView: Double,
     transformation: Matrix = Identity4x4
-)
+) {
+
+  /**
+    * Cutting the field of view in half creates a right triangle,
+    * so half-view = tan(field-of-view / 2) / distance-from-the-eye,
+    * and the canvas is only 1 unit away from the eye
+    */
+  def halfView: Double = math.tan(fieldOfView / 2)
+
+  val aspectRatio: Double = hSize / vSize
+
+  def halfWidth: Double =
+    if (aspectRatio >= 1) halfView else halfView * aspectRatio
+
+  def halfHeight: Double =
+    if (aspectRatio >= 1) halfView / aspectRatio else halfView
+
+  /**
+    * Note: A pixel is a square so the vertical size and horizontal size
+    * are the same. The formula could also be `halfHeight * 2 / vSize`
+    */
+  def pixelSize: Double =
+    halfWidth * 2 / hSize
+}
--- src/test/scala/CameraSpec.scala
+++ src/test/scala/CameraSpec.scala
@@ -1,3 +1,4 @@
+import com.melvic.esena.Math.roundTo
 import com.melvic.esena.matrix.Matrix.Identity4x4
 import com.melvic.esena.scene.Camera
 import org.scalatest.flatspec.AnyFlatSpec
@@ -10,5 +11,15 @@ class CameraSpec extends AnyFlatSpec with should.Matchers {
     camera.vSize should be (120)
     camera.fieldOfView should be (math.Pi / 2)
     camera.transformation should be (Identity4x4)
+  }
+
+  "The pixel size" should "be calculated correctly for the horizontal canvas" in {
+    val cam = Camera(200, 125, math.Pi / 2)
+    roundTo(2)(cam.pixelSize) should be (0.01)
+  }
+
+  "The pixel size" should "be calculated correctly for the vertical canvas" in {
+    val cam = Camera(125, 200, math.Pi / 2)
+    roundTo(2)(cam.pixelSize) should be (0.01)
   }
 }
