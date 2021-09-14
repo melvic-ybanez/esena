0a1306d0ac458e6794f6194f89f27889e86dbedd
Melvic Ybanez <melvic.ybanez2014@gmail.com>
2021-09-15 01:10:51 +0800

    Implement plane methods

--- src/main/scala/com/melvic/esena/shapes/Plane.scala
+++ src/main/scala/com/melvic/esena/shapes/Plane.scala
@@ -1,27 +1,49 @@
 package com.melvic.esena.shapes
+import com.melvic.esena.MathUtils
 import com.melvic.esena.lights.Material
 import com.melvic.esena.matrix.Matrix
-import com.melvic.esena.rays.Ray
+import com.melvic.esena.rays.Intersection.Intersections
+import com.melvic.esena.rays.{Intersection, Ray}
 import com.melvic.esena.tuples.{Point, Vec}
 
+/**
+  * A plane in xz, extending infinitely to both x and z directions.
+  */
 trait Plane extends Shape {
-  override type S = this.type
+  override type S = Plane
 
   /**
-   * Intersects with a transformed ray. If the ray is not
-   * transformed yet, which usually is the case, you might
-   * need to call [[intersect]] instead.
-   */
-  override def localIntersect(transformedRay: Ray) = ???
+    * Intersects with a transformed ray. If the ray is not
+    * transformed yet, which usually is the case, you might
+    * need to call [[intersect]] instead.
+    */
+  override def localIntersect(transformedRay: Ray): Intersections =
+    if (math.abs(transformedRay.direction.y) < MathUtils.Epsilon) {
+      // y is practically 0 in this case. Either the ray is parallel
+      // to the plane (in which case it misses the plane entirely)
+      // or it's coplanar with it. If it's the latter, then it technically
+      // doesn't miss the plane (in fact there would be infinite number of
+      // intersections), but it would be invisible because of how thin the plane is
+      Vector.empty
+    } else Intersection(-transformedRay.origin.y / transformedRay.direction.y, this) +: Vector()
 
   override def localNormalAt(objectPoint: Point) =
     Vec(0, 1, 0)
 
-  override def withMaterial(newMaterial: Material) = ???
+  override def withMaterial(newMaterial: Material) =
+    Plane(newMaterial, transformation)
 
-  override def withTransformation(newTransformation: Matrix) = ???
+  override def withTransformation(newTransformation: Matrix) =
+    Plane(material, newTransformation)
 }
 
-object Plane {
+object Plane extends Plane {
   def apply(): Plane = new Plane {}
+
+  def apply(initMaterial: Material, initTransformation: Matrix): Plane =
+    new Plane {
+      override def material = initMaterial
+
+      override def transformation = initTransformation
+    }
 }
--- src/test/scala/PlanesSpec.scala
+++ src/test/scala/PlanesSpec.scala
@@ -1,12 +1,13 @@
+import com.melvic.esena.rays.Ray
 import com.melvic.esena.shapes.Plane
 import com.melvic.esena.tuples.{Point, Vec}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
 
 class PlanesSpec extends AnyFlatSpec with should.Matchers {
-  "The normal of a plane" should "be constant everywhere" in {
-    val plane = Plane()
+  val plane = Plane()
 
+  "The normal of a plane" should "be constant everywhere" in {
     val n1 = plane.localNormalAt(Point.Origin)
     val n2 = plane.localNormalAt(Point(10, 0, -10))
     val n3 = plane.localNormalAt(Point(-5, 0, 150))
@@ -14,5 +15,33 @@ class PlanesSpec extends AnyFlatSpec with should.Matchers {
     n1 should be (Vec(0, 1, 0))
     n2 should be (Vec(0, 1, 0))
     n3 should be (Vec(0, 1, 0))
+  }
+
+  "A ray parallel to the plane" should "miss the plane" in {
+    val ray = Ray(Point(0, 10, 0), Vec(0, 0, 1))
+    val xs = plane.localIntersect(ray)
+    xs should be (Vector())
+  }
+
+  "A ray coplanar with the plane" should "miss the plane" in {
+    val ray = Ray(Point.Origin, Vec(0, 0, 1))
+    val xs = plane.localIntersect(ray)
+    xs should be (Vector())
+  }
+
+  "A ray intersecting a plane from above" should "compute the t as -origin/direction" in {
+    val ray = Ray(Point(0, 1, 0), Vec(0, -1, 0))
+    val xs = plane.localIntersect(ray)
+    xs.size should be (1)
+    xs.head.t should be (1)
+    xs.head.obj should be (plane)
+  }
+
+  "A ray intersecting a plane from below" should "compute the t as -origin/direction" in {
+    val ray = Ray(Point(0, -1, 0), Vec(0, 1, 0))
+    val xs = plane.localIntersect(ray)
+    xs.size should be (1)
+    xs.head.t should be (1)
+    xs.head.obj should be (plane)
   }
 }
