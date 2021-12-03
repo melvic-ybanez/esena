73b8adb1b416128638ab001176020754ec29bc87
Melvic Ybanez <melvic.ybanez2014@gmail.com>
2021-12-04 01:38:36 +0800

    Add simple logs during rendering

--- src/main/scala/com/melvic/esena/Main.scala
+++ src/main/scala/com/melvic/esena/Main.scala
@@ -5,9 +5,15 @@ import java.nio.file.{Files, Paths}
 
 object Main {
   def main(args: Array[String]): Unit = {
+    println("Rendering...")
+    val start = System.nanoTime()
     val canvas = MainScene.build
+    val end = System.nanoTime()
+    println(s"Rendering completed. Execution time: ${(end - start) / 1000 / 1000 / 1000} seconds")
 
-    // write to file
-    Files.write(Paths.get("esena.ppm"), canvas.ppm.toString.getBytes(StandardCharsets.UTF_8))
+    println(s"Writing to file...")
+    val filename = "esena.ppm"
+    Files.write(Paths.get(filename), canvas.ppm.toString.getBytes(StandardCharsets.UTF_8))
+    println(s"Writing completed. Filename: $filename")
   }
 }
